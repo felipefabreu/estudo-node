cf2396a213cb9625cab4aafd5709a4c9fd7ceb36
Felipe <felipefabreu@gmail.com>
2022-05-15 19:28:51 -0300

    resolving typeorm version and adding migration

---
+++ src/database/migrations/1652653704524-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAppointments1652653704524 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
